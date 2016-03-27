f602cd2a616649b1b379c77e5878e821466a8c55
rashed <rashed.hacker8271@gmail.com>
2016-03-27 14:52:23 -0700

    Update echo.lua

--- plugins/echo.lua
+++ plugins/echo.lua
@@ -11,9 +11,9 @@ end
 
 return {
   description = "Simplest plugin ever!",
-  usage = "!echo [whatever]: echoes the msg",
+  usage = "echo [whatever]: echoes the msg",
   patterns = {
-    "^!echo(.+)$"
+    "^echo(.+)$"
   }, 
   run = run 
 }
