d9bf87beed380cbdb0b2ed9fa36fa3c20e1851f6
rashed <rashed.hacker8271@gmail.com>
2016-03-27 15:24:36 -0700

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -138,11 +138,11 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[!/]([Ss]tats)$",
-    "^[!/]([Ss]tatslist)$",
-    "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (avast)",-- Put everything you like :)
-		"^[!/]([Aa]vast)"-- Put everything you like :)
+    "^([Ss]tats)$",
+    "^([Ss]tatslist)$",
+    "^([Ss]tats) (group) (%d+)",
+    "^([Ss]tats) (avast)",-- Put everything you like :)
+		"^([Aa]vast)"-- Put everything you like :)
     }, 
   run = run
 }
